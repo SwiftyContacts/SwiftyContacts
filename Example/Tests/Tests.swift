95b811dcc6e8baee39b37fe8c96cea989c4e899f
Satish Babariya <satish.babariya@gmail.com>
2017-09-02 11:43:04 -0700

    no message

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,45 +6,17 @@ import SwiftyContacts
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
-//        describe("these will fail") {
-//
-//            it("can do maths") {
-//                expect(1) == 2
-//            }
-//
-//            it("can read") {
-//                expect("number") == "string"
-//            }
-//
-//            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-//            }
-//            
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
-//        }
+        describe("these will fail") {
+
+            describe("Request Acess") {
+                it("Request", closure: {
+                    
+                    requestAccess({ (bool) in
+                        expect(true || false) == bool
+                    })
+                })
+            }
+            
+        }
     }
 }
