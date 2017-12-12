a36a5b34a4bfd47245d271e926844e99a497c1ac
Satish Babariya <satish.babariya@gmail.com>
2017-12-12 16:42:13 +0530

    Added Docs

---
+++ build/SwiftyContacts.build/Release-iphoneos/SwiftyContacts-iOS.build/DerivedSources/SwiftyContacts_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char SwiftyContactsVersionString[];
+ extern const double SwiftyContactsVersionNumber;
+
+ const unsigned char SwiftyContactsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftyContacts  PROJECT:SwiftyContacts-1" "\n";
+ const double SwiftyContactsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/SwiftyContacts.build/Release-iphoneos/SwiftyContacts-iOS.build/Objects-normal/arm64/SwiftyContacts_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/wmt_ios_3/Desktop/SwiftyContacts/build/SwiftyContacts.build/Release-iphoneos/SwiftyContacts-iOS.build/DerivedSources/SwiftyContacts_vers.c
