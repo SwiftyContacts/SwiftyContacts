4f9cfe28ab2998eb4c8471429622936dd684ceb1
Satish B <satish.babariya@gmail.com>
2020-10-05 14:22:39 +0530

    added some test cases

--- Tests/SwiftyContactsTests/SwiftyContactsTests.swift
+++ Tests/SwiftyContactsTests/SwiftyContactsTests.swift
@@ -1,15 +1,44 @@
-import XCTest
 @testable import SwiftyContacts
+import XCTest
 
 final class SwiftyContactsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(SwiftyContacts().text, "Hello, World!")
+    func testRequestAccess() {
+        let e = expectation(description: "requestAccess")
+
+        requestAccess { bool in
+            XCTAssertEqual(bool, true)
+            e.fulfill()
+        }
+
+        waitForExpectations(timeout: 1.0) { error in
+            if let error = error {
+                XCTFail("requestAccess errored: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func testFetchContacts() {
+        let e = expectation(description: "fetchContacts")
+
+        fetchContacts { result in
+            switch result {
+            case let .success(contacts):
+                XCTAssertEqual(contacts.count >= 0, true)
+            case let .failure(error):
+                XCTFail("fetchContacts errored: \(error.localizedDescription)")
+            }
+            e.fulfill()
+        }
+
+        waitForExpectations(timeout: 1.0) { error in
+            if let error = error {
+                XCTFail("fetchContacts errored: \(error.localizedDescription)")
+            }
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testRequestAccess", testRequestAccess),
+        ("testFetchContacts", testFetchContacts),
     ]
 }
