ff321b3ee920d1c9bf3d8488e160bd5168fab392
Satish Babariya <satish.babariya@gmail.com>
2021-12-18 00:52:39 +0530

    add encode decoder

---
+++ Sources/SwiftyContacts/SwiftyContacts+VCard.swift
@@ -0,0 +1,37 @@
+//    Copyright (c) 2022 Satish Babariya <satish.babariya@gmail.com>
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+
+@_exported import Contacts
+
+/// Returns the vCard representation of the specified contacts.
+/// - Parameter contacts: An array of contacts.
+/// - Throws: Contains error information.
+/// - Returns: An NSData object with the vCard representation of the contact.
+public func encode(contacts: [CNContact]) throws -> Data {
+    return try CNContactVCardSerialization.data(with: contacts)
+}
+
+/// Returns the contacts from the vCard data.
+/// - Parameter data: The vCard data representing one or more contacts.
+/// - Throws: Error information.
+/// - Returns: An array of contacts.
+public func decode(data: Data) throws -> [CNContact] {
+    return try CNContactVCardSerialization.contacts(with: data)
+}
