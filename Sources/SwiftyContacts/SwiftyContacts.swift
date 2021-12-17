c8c9a11eb06a322312bab9e5e34b1840258bf9cc
Satish Babariya <satish.babariya@gmail.com>
2021-12-17 18:16:51 +0530

    add fetchContacts with predicate

--- Sources/SwiftyContacts/SwiftyContacts.swift
+++ Sources/SwiftyContacts/SwiftyContacts.swift
@@ -61,3 +61,73 @@ public func fetchContacts(keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerial
         }
     }
 }
+
+/// fetch contacts matching a conditions.
+/// - Parameters:
+///   - predicate: A definition of logical conditions for constraining a search for a fetch or for in-memory filtering.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+private func fetchContacts(predicate: NSPredicate, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
+}
+
+/// fetch contacts matching a name.
+/// - Parameters:
+///   - name: The name can contain any number of words.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(matchingName name: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContacts(matchingName: name), keysToFetch: keysToFetch)
+}
+
+/// Fetch contacts matching an email address.
+/// - Parameters:
+///   - emailAddress: The email address to search for. Do not include a scheme (e.g., "mailto:").
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(matchingEmailAddress emailAddress: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContacts(matchingEmailAddress: emailAddress), keysToFetch: keysToFetch)
+}
+
+/// Fetch contacts matching a phone number.
+/// - Parameters:
+///   - phoneNumber: A CNPhoneNumber representing the phone number to search for. Do not include a scheme (e.g., "tel:").
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(matching phoneNumber: CNPhoneNumber, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContacts(matching: phoneNumber), keysToFetch: keysToFetch)
+}
+
+/// To fetch contacts matching contact identifiers.
+/// - Parameters:
+///   - identifiers: Contact identifiers to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(withIdentifiers identifiers: [String], keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContacts(withIdentifiers: identifiers), keysToFetch: keysToFetch)
+}
+
+/// To fetch contacts matching group identifier
+/// - Parameters:
+///   - groupIdentifier: The group identifier to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(withGroupIdentifier groupIdentifier: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContactsInGroup(withIdentifier: groupIdentifier), keysToFetch: keysToFetch)
+}
+
+/// find the contacts in the specified container.
+/// - Parameters:
+///   - containerIdentifier: The container identifier to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Throws: Error information, if an error occurred.
+/// - Returns: Array  of contacts
+public func fetchContacts(withContainerIdentifier containerIdentifier: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()]) throws -> [CNContact] {
+    return try CNContactStore().unifiedContacts(matching: CNContact.predicateForContactsInContainer(withIdentifier: containerIdentifier), keysToFetch: keysToFetch)
+}
