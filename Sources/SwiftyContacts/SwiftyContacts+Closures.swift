72b6ad92a760654b0926c99a8f9f879a4ab968e7
Satish Babariya <satish.babariya@gmail.com>
2021-12-17 23:08:15 +0530

    fix method conflict

--- Sources/SwiftyContacts/SwiftyContacts+Closures.swift
+++ Sources/SwiftyContacts/SwiftyContacts+Closures.swift
@@ -57,3 +57,123 @@ public func fetchContacts(keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerial
         completion(.failure(error))
     }
 }
+
+/// fetch contacts matching a conditions.
+/// - Parameters:
+///   - predicate: A definition of logical conditions for constraining a search for a fetch or for in-memory filtering.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+private func fetchContacts(predicate: NSPredicate, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// fetch contacts matching a name.
+/// - Parameters:
+///   - name: The name can contain any number of words.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(matchingName name: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContacts(matchingName: name), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// Fetch contacts matching an email address.
+/// - Parameters:
+///   - emailAddress: The email address to search for. Do not include a scheme (e.g., "mailto:").
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(matchingEmailAddress emailAddress: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContacts(matchingEmailAddress: emailAddress), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// Fetch contacts matching a phone number.
+/// - Parameters:
+///   - phoneNumber: A CNPhoneNumber representing the phone number to search for. Do not include a scheme (e.g., "tel:").
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(matching phoneNumber: CNPhoneNumber, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContacts(matching: phoneNumber), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// To fetch contacts matching contact identifiers.
+/// - Parameters:
+///   - identifiers: Contact identifiers to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(withIdentifiers identifiers: [String], keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContacts(withIdentifiers: identifiers), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// To fetch contacts matching group identifier
+/// - Parameters:
+///   - groupIdentifier: The group identifier to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(withGroupIdentifier groupIdentifier: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContactsInGroup(withIdentifier: groupIdentifier), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// find the contacts in the specified container.
+/// - Parameters:
+///   - containerIdentifier: The container identifier to be matched.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: returns array of contacts
+/// on error: error information, if an error occurred.
+public func fetchContacts(withContainerIdentifier containerIdentifier: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<[CNContact], Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContacts(matching: CNContact.predicateForContactsInContainer(withIdentifier: containerIdentifier), keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
+
+/// Fetch a  contact with a given identifier.
+/// - Parameters:
+///   - identifier: The identifier of the contact to fetch.
+///   - keysToFetch: The contact fetch request that specifies the search criteria.
+/// - Returns: returns either a success or a failure,
+/// on sucess: contact matching or linked to the identifier
+/// on error: error information, if an error occurred.
+private func fetchContact(withIdentifier identifier: String, keysToFetch: [CNKeyDescriptor] = [CNContactVCardSerialization.descriptorForRequiredKeys()], _ completion: @escaping (Result<CNContact, Error>) -> Void) {
+    do {
+        completion(.success(try ContactStore.default.unifiedContact(withIdentifier: identifier, keysToFetch: keysToFetch)))
+    } catch {
+        completion(.failure(error))
+    }
+}
